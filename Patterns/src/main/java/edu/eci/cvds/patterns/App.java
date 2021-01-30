e7fcfc52c9cb57c1ff5d16049a30731d9a0e4b29
Ricardo1335 <ricardo.amaya@mail.escuelaing.edu.co>
2021-01-30 10:39:39 -0500

    Esqueleto

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	if (args.length<1) {
+            System.out.println( "Hello World!" );
+    	}else { 
+        	System.out.println( "Hello "+ args[0]+"!" );    
+    
+    	}
+
     }
 }
